8985bca8bd02363e806df16ef0421ae7133b013c
SanderHansen <ssh@tv2.dk>
2013-08-23 13:16:58 +0200

    Final demo version for 8.23.2013

--- Main.js
+++ Main.js
@@ -348,6 +348,9 @@ $(function(){
     //initialize the start button
     $("#startbutton").click(function(){
         $.playground().startGame(function(){
+			var spinner = new Spinner().spin();
+				  background.appendChild(spinner.el);
+
             $("#welcomeScreen").remove();
 			$("#background").playSound();
         });
@@ -372,4 +375,38 @@ $(function(){
 	$("#PointHUD").html("Points: "+Points);	
 	
     }, REFRESH_RATE);
+	
+	$.fn.spin = function(opts) {
+		this.each(function() {
+			var $this = $(this),
+			spinner = $this.data('spinner');
+
+			if (spinner) spinner.stop();
+			if (opts !== false) {
+			opts = $.extend({color: $this.css('color')}, opts);
+			spinner = new Spinner(opts).spin(this);
+			$this.data('spinner', spinner);
+			}
+			});
+			return this;
+			};
+			$(function() {
+			$(".spinner-link").click(function(e) {
+			e.preventDefault();
+			$(this).hide();
+			var opts = {
+			lines: 12, // The number of lines to draw
+			length: 7, // The length of each line
+			width: 5, // The line thickness
+			radius: 10, // The radius of the inner circle
+			color: '#fff', // #rbg or #rrggbb
+			speed: 1, // Rounds per second
+			trail: 66, // Afterglow percentage
+			shadow: true // Whether to render a shadow
+			};
+			$("#spin").show().spin(opts);
+
+		});
+	});
+
 });
